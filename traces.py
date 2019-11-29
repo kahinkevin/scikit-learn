3aae7c715f37eb5ae8bc4ae4aa04cb6b799b945e
Kevin Chan <you@example.com>
2019-11-29 11:13:13 -0500

    remove sk_not_calling_array_function to keep template clean

--- traces.py
+++ traces.py
@@ -17,18 +17,4 @@ print("TRACER WAS CALLED")
 with open("/results/tracelog_(exp)_corrected_" + model_name + ".txt", "a") as myfile:
     myfile.write(model_name + " in (file) line (nb) called \n")
 
-________________________________________________________
-sk_not_calling_array_function
-
-import os
-model_name = ""
-print("TRACER WAS CALLED")
-with open("/results/tracelog_(exp)_buggy_" + model_name + ".txt", "a") as myfile:
-    myfile.write(model_name + " in (file) line (nb) called \n")
-
-import os
-model_name = ""
-print("TRACER WAS CALLED")
-with open("/results/tracelog_(exp)_corrected_" + model_name + ".txt", "a") as myfile:
-    myfile.write(model_name + " in (file) line (nb) called \n")
 
