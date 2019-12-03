48308e93120c8110a51ee4e450bbfb03fdabaa18
Kevin Chan <you@example.com>
2019-12-03 10:39:18 -0500

    correct tracelogs path

--- template_traces.py
+++ template_traces.py
@@ -1,20 +1,21 @@
 # NOT A RUNNING A FILE
 
 import os
+model_name = ""
 print("TRACER WAS CALLED")
-with open("/results/tracelog_(exp)_(evalType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
+with open("/home/kacham/Documents/tracelogs/tracelog_(exp)_(evalType)_" + os.environ['MODEL_NAME'] + ".txt", "a") as myfile:
     myfile.write(os.environ['MODEL_NAME'] + " in (file) line (nb) called \n")
 
 import os
 model_name = ""
 print("TRACER WAS CALLED")
-with open("/results/tracelog_(exp)_buggy_" + model_name + ".txt", "a") as myfile:
-    myfile.write(model_name + " in (file) line (nb) called \n")
+with open("/home/kacham/Documents/tracelogs/tracelog_(exp)_buggy_" + model_name + ".txt", "a") as myfile:
+    myfile.write(model_name + " in  line  called \n")
 
 import os
 model_name = ""
 print("TRACER WAS CALLED")
-with open("/results/tracelog_(exp)_corrected_" + model_name + ".txt", "a") as myfile:
-    myfile.write(model_name + " in (file) line (nb) called \n")
+with open("/home/kacham/Documents/tracelogs/tracelog_(exp)_corrected_" + model_name + ".txt", "a") as myfile:
+    myfile.write(model_name + " in  line  called \n")
 
 
