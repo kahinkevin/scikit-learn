2dc98c132ab57066aec7b3aa3836792d2fde08f8
Kevin Chan <you@example.com>
2019-12-09 13:12:50 -0500

    adapted training script for continuous variables

--- train.py
+++ train.py
@@ -11,19 +11,17 @@ from sklearn.linear_model import BayesianRidge
 
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error, mean_squared_error, precision_score, r2_score, recall_score
+from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, r2_score
 
 # load data
 dataset_url = '/home/kacham/Documents/datasets_kaggle/continuous/2017.csv'
 data = pd.read_csv(dataset_url)
 
 # separate features
-y_features = ['cnt']
+y_features = ['Health_Life_Expectancy']
 y = data[y_features]
-y = y.astype('int') #fix 1
-X_features = ['t1', 't2', 'hum', 'wind_speed', 'weather_code', 'is_holiday', 'is_weekend', 'season']
+X_features = ['Happiness_Score', 'Economy_GDP_per_Capital', 'Family', 'Freedom', 'Generosity', 'Trust_Government_Corruption', 'Dystopia_Residual']
 X = data[X_features]
-X = X.astype('int') #fix 1
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, 
                                                     test_size=0.2, 
@@ -35,7 +33,7 @@ model = BayesianRidge()
 model.fit(X_train, y_train.values.ravel()) # fix 3
 
 # predict Output
-predicted_new_bike_shares = model.predict(X_test)
+predicted_health_life_expectancy = model.predict(X_test)
 
 # stats
 print('--- comparison of real value and pred ---')
@@ -43,17 +41,14 @@ print(model)
 print('    true:')
 print(y_test)
 print('\n    predicted:')
-print(predicted_new_bike_shares)
+print(predicted_health_life_expectancy)
 print('-----------------------------------------')
 
-acc = accuracy_score(y_test, predicted_new_bike_shares)
-f1 = f1_score(y_test, predicted_new_bike_shares, average='macro')
-mae = mean_absolute_error(y_test, predicted_new_bike_shares)
-mse = mean_squared_error(y_test, predicted_new_bike_shares)
-pr = precision_score(y_test, predicted_new_bike_shares, average='macro')
-r2 = r2_score(y_test, predicted_new_bike_shares)
-rec = recall_score(y_test, predicted_new_bike_shares, average='macro')
-metrics_msg = 'accuracy: {} - f1: {} - mean absolute errror: {} - mean squared error: {} \n - precision: {} - r2 score: {} - recall: {} \n \n'.format(acc, f1, mae, mse, pr, r2, rec)
+evs = explained_variance_score(y_test, predicted_health_life_expectancy)
+mae = mean_absolute_error(y_test, predicted_health_life_expectancy)
+mse = mean_squared_error(y_test, predicted_health_life_expectancy)
+r2 = r2_score(y_test, predicted_health_life_expectancy)
+metrics_msg = 'evs: {} - mean absolute errror: {} - mean squared error: {} - r2 score: {} \n \n'.format(evs, mae, mse, r2)
 # "/home/kacham/Documents/tracelogs/metrics/(exp)_(challenge)_(model)_buggy/corrected_metrics.txt"
 # TODO ------------------------------------------------------------- update for each exp
 with open("/home/kacham/Documents/tracelogs/metrics/sk_fix_br_regression_world-happiness_BayesianRidge_buggy_metrics.txt", "a") as myfile:
