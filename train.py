e8f92a5ba0bc76d6bbddd2aa1aeb3c875c921345
Kevin Chan <you@example.com>
2019-12-11 13:11:41 -0500

    correct the buggy params' filename

--- train.py
+++ train.py
@@ -56,9 +56,9 @@ rec = recall_score(y_test, predicted_new_bike_shares, average='macro')
 metrics_msg = 'accuracy: {} - f1: {} - mean absolute errror: {} - mean squared error: {} \n - precision: {} - r2 score: {} - recall: {} \n \n'.format(acc, f1, mae, mse, pr, r2, rec)
 # "/home/kacham/Documents/tracelogs/metrics/(exp)_(challenge)_(model)_buggy/corrected_metrics.txt"
 # TODO ------------------------------------------------------------- update for each exp
-with open("/home/kacham/Documents/tracelogs/metrics/sk_fix_alpha_0_london_merged_BaseDiscreteNB_buggy_metrics.txt", "a") as myfile:
+with open("/home/kacham/Documents/tracelogs/metrics/sk_fix_alpha_0_london_merged_BernoulliNB_buggy_metrics.txt", "a") as myfile:
     myfile.write(metrics_msg)
-with open("/home/kacham/Documents/tracelogs/params/sk_fix_alpha_0_world-happiness_BaseDiscreteNB_buggy_params.txt", "a") as myfile:
+with open("/home/kacham/Documents/tracelogs/params/sk_fix_alpha_0_london_merged_BernoulliNB_buggy_params.txt", "a") as myfile:
     print(model,file=myfile)
     myfile.write('True:')
     print(y_test,file=myfile)
